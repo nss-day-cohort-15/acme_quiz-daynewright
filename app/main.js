14fdb15908b931a3c3a8d4baa2aef682495ee519
Dayne Wright <daynewr@gmail.com>
2016-08-03 14:37:45 -0500

    updated styling and structure

--- app/main.js
+++ app/main.js
@@ -14,17 +14,16 @@ $(document).ready(function(){
             })
      },
      addProdDom : function(product, type, category) {
-            $('#store-items').append(`
-                        <div class="col-md-6 center">
-                            <h2>${product.name}</h2>
-                            <h4><span>Type:</span> ${type[product.type].name} |<span> Category:</span> ${category}</h4>
-                            <hr />
-                            <p>${product.description}</p>
-                        </div>
+        $('#store-items').append(`
+             <div class="col-md-6 center prod">
+                <h2>${product.name}</h2>
+                <h4><span>Type:</span> ${type[product.type].name} |<span> Category:</span> ${category}</h4>
+                <hr />
+                <p>${product.description}</p>
+              </div>
                     `);
      }
    }
-
 
    function getProducts(){
 
@@ -36,8 +35,9 @@ $(document).ready(function(){
      $.getJSON('app/data/types.json')
         .then(function(res){
           types = res.types;
-           $.getJSON('app/data/products.json')
-                .then(function({products}){
+        }).then(function () {
+           return $.getJSON('app/data/products.json')
+        }).then(function({products}){
                   types.forEach(type  => {
                    for(product in products[0]){
                      if(selectedId === type.category && products[0][product].type  === type.id){
@@ -45,10 +45,8 @@ $(document).ready(function(){
                      }
                    }
                   });
-                });
-        });
+         });
    }
-
 
   Store.addCatDom();
 
